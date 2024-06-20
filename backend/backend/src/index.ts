ed98a9e69f6b9500257241159935658c744e820a
retrophozeac <f20190899@pilani.bits-pilani.ac.in>
2024-06-20 11:07:26 +0530

    hono setup done

--- backend/backend/src/index.ts
+++ backend/backend/src/index.ts
@@ -1,11 +1,22 @@
 import { Hono } from 'hono'
-const app = new Hono()
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
 
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 app.get('/', async (c) => {
+    
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate())
+    console.log("start");
     const allUsers = await prisma.orders.findMany();
-    return c.json(allUsers);
+    console.log(allUsers);
+    return c.json({msg:"done"});
   })
   
   export default app
